e2f1be0adb3e67a3850c4d0c5f061ec59ad526cd
ritesh <riteshsaheb@gmail.com>
2018-11-16 23:31:29 -0800

    70% done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,20 +14,30 @@ $(document).ready(function(){
         a3: "Scary spice",
         a4: "Posh Spice"
     },
-    {
-    que: "Which NBA team won the most titles in the 90s?", 
+    { que: "Which NBA team won the most titles in the 90s?", 
     a1: "New York Knicks",
     a2: "Portland Trailblazers",
     a3: "Chicago Bulls",
     a4: "Los Angeles Lakers"
     },
-    {
-    que: "Which Group released the hit song, “Smells Like Teem Spirit”?", 
+    { que: "Which Group released the hit song, “Smells Like Teem Spirit”?", 
     a1: "No Doubt",
     a2: "The Offspring",
     a3: "Backstreet Boys",
     a4: "Nirvana"
-    }];
+    },
+    { que: "Which popular Disney movie featured the song, “Circle of Life”?",
+    a1: "Aladdin",
+    a2: "Hercules",
+    a3: "Mulan",
+    a4: "The Lion King"
+    },
+    { que: "What does Doug’s best friend’s name?",
+    a1: "Skeeter",
+    a2: "Mark",
+    a3: "Zach",
+    a4: "Cody"
+  }];
 
     var image = ["assets/images/loading.gif"];
     var rightAns = 0;
@@ -45,12 +55,9 @@ $(document).ready(function(){
         var notAns = 0;
         var queCount = 0;
         var time = 30;
-        // var newDiv = $("<div class = 'restart'>");
-        // newDiv.append(`<button data-value = 'startagain'> Start Game </button>`);
-        // $("#display").append(newDiv);
+        stop();
         run();
-          
-    };
+      };
     function run() {
         if(!clockRunning){
         clearInterval(intervalId);
@@ -61,88 +68,118 @@ $(document).ready(function(){
       };
     function decrement() {
         $("#display").empty();
-        
         time--;
-        // Show the time in display  tag.
-        //  after 30 seconds hits zero...
         if (time === 0) {
             time = 30;
           stop(); //  ...run the stop function.
           queCount++;
           displayQuestion();
-          run();
-          //  Alert the user that time is up.
-          //alert("Time Up!");
+          //run();
         };
         $("#display").text(`Time Remaining : ${time} Seconds`);
-        
       };
     function stop() {
         clockRunning = false;
         clearInterval(intervalId);
       };
-
-    //   function Msg1(){
-    //     document.getElementById('#ansbutton1').innerHTML = quizQuestions[queCount].a1;
-    //   }
-    
     
       function displayQuestion(){
-        if(queCount <= quizQuestions.length){
+        if(queCount < quizQuestions.length){
           $("#quenumber").empty();
           // $("#display").text(`Time Remaining : ${time} Seconds`)
           // document.querySelector("#quenumber").innerHTML = quizQuestions[queCount].que;
           // document.querySelector("#ansbutton1").innerHTML = quizQuestions[queCount].a1;
-          // $("#ansbutton1").attr("data-answer", quizQuestions[i].a1);
-          // document.querySelector("#ansbutton2").innerHTML = quizQuestions[queCount].a2;
-          // $("#ansbutton2").attr("data-answer", quizQuestions[i].a2);
-          // document.querySelector("#ansbutton3").innerHTML = quizQuestions[queCount].a3;
-          // $("#ansbutton3").attr("data-answer", quizQuestions[i].a3);
-          // document.querySelector("#ansbutton4").innerHTML = quizQuestions[queCount].a4;
-          // $("#ansbutton4").attr("data-answer", quizQuestions[i].a4);
           var newDiv = $("<div class = 'question'>");
           newDiv.append(`<p> ${quizQuestions[queCount].que}</p>`);
-          newDiv.append(`<button data-value = '${quizQuestions[queCount].a1}'> ${quizQuestions[queCount].a1} </button>`);
+          newDiv.append(`<button class = 'button' data-value = '${quizQuestions[queCount].a1}'> ${quizQuestions[queCount].a1} </button>`);
           newDiv.append(`<br>`);
-          newDiv.append(`<button data-value = '${quizQuestions[queCount].a2}'> ${quizQuestions[queCount].a2} </button>`);
+          newDiv.append(`<button class = 'button' data-value = '${quizQuestions[queCount].a2}'> ${quizQuestions[queCount].a2} </button>`);
           newDiv.append(`<br>`);
-          newDiv.append(`<button data-value = '${quizQuestions[queCount].a3}'> ${quizQuestions[queCount].a3} </button>`);
+          newDiv.append(`<button class = 'button' data-value = '${quizQuestions[queCount].a3}'> ${quizQuestions[queCount].a3} </button>`);
           newDiv.append(`<br>`);
-          newDiv.append(`<button data-value = '${quizQuestions[queCount].a4}'> ${quizQuestions[queCount].a4} </button>`);
+          newDiv.append(`<button class = 'button' data-value = '${quizQuestions[queCount].a4}'> ${quizQuestions[queCount].a4} </button>`);
           $("#quenumber").append(newDiv);
-
-        }else{
-          document.querySelector("#quenumber").innerHTML = "Game Over";
-          //document.querySelector("#score").innerHTML = "Final Score " + score + " out of " + questions.length;
         }
+        else{
+          $("#display").text(`Time Remaining : ${time} Seconds`).hide();
+          stop();
+          $("#display").empty();
+          $("#quenumber").empty();
+          $("#imgholder").empty();
+          var newDiv1 = $("<div class = 'restart'>");
+          newDiv1.append("<button id = 'startagain'> Start Game </button>");
+          newDiv1.append("<br>");
+          newDiv1.append(`<p>Right Answer : ${rightAns} </p>`);
+          newDiv1.append(`<p>Wrong Answer : ${wrongAns} </p>`);
+          $("#quenumber").append(newDiv1);
+          console.log("question finish and div empty");
+         }
       };
 
+      function displayGif() {
+        $("#imgholder").html("<img src='loading.gif' width='400px'>");
+      }
 
 $(document).on("click","#startgame",function(){
         $(this).hide();
         $("#display").text(`Time Remaining : ${time} Seconds`).show();
-        // $("#ansbutton1").show();
-        // $("#ansbutton2").show();
-        // $("#ansbutton3").show();
-        // $("#ansbutton4").show();
-        console.log("clicked");
         run();
-        console.log("run");
-        //displayQuestion();
-        //displayAns();
-        console.log("displayQuestion");
-        $(".button").on("click", function(){
-
+          $(document).on("click",".button", function(event){
+            event.preventDefault();
+            //$(this).attr("#data-id")
+            var buttonVal = $(this).attr("data-value");
+            //console.log($(this).attr("data-value"));
+            console.log(buttonVal);
+            if((buttonVal === "The Toy Story")||(buttonVal === "Fred Spice")||(buttonVal === "Chicago Bulls")
+            ||(buttonVal === "Nirvana")||(buttonVal === "The Lion King")||(buttonVal === "Skeeter")){
+              $("#quenumber").empty();
+              $("#quenumber").text("Correct!!!");
+              $("#imgholder").append("<img src='loading.gif' width='400px'>");
+              rightAns++;
+              time = 30;
+              stop();
+              setTimeout(run, 2000);
+              $("#imgholder").empty();
+              queCount++;
+            }else{
+              $("#quenumber").empty();
+              $("#quenumber").text("Wrong!!!");
+              $("#imgholder").append("<img src='loading.gif' width='400px'>");
+              wrongAns++;
+              time = 30;
+              stop();
+              setTimeout(run, 2000);
+              $("#imgholder").empty();
+              queCount++;
+            };
+          
         });      
 
 });
 
-if(queCount === quizQuestions.length){
-  displayQuestion().hide();
-  $("#display").empty();
-  var newDiv = $("<div class = 'restart'>");
-  newDiv.append(`<button data-value = 'startagain'> Start Game </button>`);
-  $("#display").append(newDiv);
-};
+$(document).on("click","#startagain", function(event){
+  event.preventDefault();
+  $(".restart").empty();
+  $("#quenumber").empty();
+  $("#imgholder").empty();
+  $(this).empty(); 
+  $(this).hide();
+  time = 30;
+  stop();       
+  resetGame();
+  run();
+});
+// if(queCount === quizQuestions.length){
+//   //displayQuestion().hide();
+//   $("#display").text(`Time Remaining : ${time} Seconds`).hide();
+//   stop();
+//   $("#display").empty();
+//   $("#quenumber").empty();
+//   $("#imgholder").empty();
+//   var newDiv1 = $("<div class = 'restart'>");
+//   newDiv1.append(`<button data-value = 'startagain'> Start Game </button>`);
+//   $("#quenumber").append(newDiv1);
+//   console.log("question finish and div empty");
+// }; 
 
 });
